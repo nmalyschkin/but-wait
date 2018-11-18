b0053f7aee29964e2f1006cfa874f4da793e0989
nmalyschkin <nikitamalyschkin@googlemail.com>
2018-11-18 17:55:15 +0100

    fix naming

--- index.js
+++ index.js
@@ -9,18 +9,19 @@ var thereIsMore = function(more) {
     if (_noMore) throw new Error("but-wait: you must not add more after noMore was called");
     if (!(more instanceof Function))
         throw new Error("but-wait: 'thereIsMore' must be called with a function that return a Promise");
-    stackOfMore.push(more);
+    _stackOfMore.push(more);
 };
 
 var noMore = function() {
     _noMore = true;
-    return Promise.all([
-        stackOfMore.map(function(more) {
+    return Promise.all(
+        _stackOfMore.map(function(more) {
             var prom = more();
+            if (!prom) throw new Error("but-wait: expected Promise as return, got undefined");
             if (prom instanceof Promise) return prom;
             else throw new Error("but-wait: expected Promise as return, got " + prom.constructor.name);
         })
-    ]);
+    ).then(() => console.log("noMore is done"));
 };
 
 module.exports = {
