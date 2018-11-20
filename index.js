4337dd35181863f512a84e3e8b9a80e30ebe0977
nmalyschkin <nikitamalyschkin@googlemail.com>
2018-11-20 22:11:09 +0100

    add eslint

--- index.js
+++ index.js
@@ -1,32 +1,31 @@
-var _stackOfMore = [];
-var _noMore = false;
+var _stackOfMore = []
+var _noMore = false
 
 /**
  * add a Promise or function that returns a Promise
- * 
+ *
  * @param {function/Promise} more if function called without any parameters, expected to return Promise
  */
-var thereIsMore = function(more) {
-    if (_noMore) throw new Error("but-wait: you must not add more after noMore was called");
-    var toPush = more;
-    if (more instanceof Function) toPush = more();
-    if (!(toPush instanceof Promise))
-        throw new Error("but-wait: expected Promise as return, got " + toPush.constructor.name);
+var thereIsMore = function (more) {
+    if (_noMore) throw new Error('but-wait: you must not add more after noMore was called')
+    var toPush = more
+    if (more instanceof Function) toPush = more()
+    if (!(toPush instanceof Promise)) { throw new Error('but-wait: expected Promise as return, got ' + toPush.constructor.name) }
 
-    _stackOfMore.push(toPush);
-};
+    _stackOfMore.push(toPush)
+}
 
 /**
  * call when no more should be added
- * 
+ *
  * @returns {Promise} Promise that is resolved when all asynchronous initializations finished
  */
-var noMore = function() {
-    _noMore = true;
-    return Promise.all(_stackOfMore);
-};
+var noMore = function () {
+    _noMore = true
+    return Promise.all(_stackOfMore)
+}
 
 module.exports = {
     thereIsMore: thereIsMore,
     noMore: noMore
-};
+}
--- test/index.js
+++ test/index.js
@@ -1,26 +1,26 @@
-const butWait = require("../index");
-const assert = require('assert').strict;
+const butWait = require('../index')
+const assert = require('assert').strict
 
-let flag = 0;
+let flag = 0
 
 // to be resolved
-let resolve1;
-const prom1 = new Promise( res => {resolve1 = res} ).then(() => {
-    assert.ok(flag === 0);
-    flag = 1;
-});
-butWait.thereIsMore(prom1);
+let resolve1
+const prom1 = new Promise(resolve => { resolve1 = resolve }).then(() => {
+    assert.ok(flag === 0)
+    flag = 1
+})
+butWait.thereIsMore(prom1)
 
 // already resolved
-const prom2 = new Promise( res => res() );
-butWait.thereIsMore(prom2);
+const prom2 = new Promise(resolve => resolve())
+butWait.thereIsMore(prom2)
 
 // more is not a promise
 assert.throws(() => {
     butWait.thereIsMore(() => {})
 })
 assert.throws(() => {
-    butWait.thereIsMore("test")
+    butWait.thereIsMore('test')
 })
 assert.throws(() => {
     butWait.thereIsMore(true)
@@ -28,21 +28,21 @@ assert.throws(() => {
 
 // finish
 butWait.noMore().then(() => {
-    assert.ok(flag === 1);
-    flag = 2;
-});
+    assert.ok(flag === 1)
+    flag = 2
+})
 
 // throw if noMore was called already
 assert.throws(() => {
     butWait.thereIsMore(() => {})
 })
 
-resolve1();
-new Promise(res => res())
+resolve1()
+new Promise(resolve => resolve())
     .then(() => {
-        assert.ok(flag === 1);    
+        assert.ok(flag === 1)
     })
     .then() // noMore() gets resolved
     .then(() => {
-        assert.ok(flag === 2);    
+        assert.ok(flag === 2)
     })
