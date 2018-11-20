113b707ba1826756995e22bce14e744b7fa2fb53
nmalyschkin <nikitamalyschkin@googlemail.com>
2018-11-20 22:00:42 +0100

    add tests

---
+++ test/index.js
@@ -0,0 +1,48 @@
+const butWait = require("../index");
+const assert = require('assert').strict;
+
+let flag = 0;
+
+// to be resolved
+let resolve1;
+const prom1 = new Promise( res => {resolve1 = res} ).then(() => {
+    assert.ok(flag === 0);
+    flag = 1;
+});
+butWait.thereIsMore(prom1);
+
+// already resolved
+const prom2 = new Promise( res => res() );
+butWait.thereIsMore(prom2);
+
+// more is not a promise
+assert.throws(() => {
+    butWait.thereIsMore(() => {})
+})
+assert.throws(() => {
+    butWait.thereIsMore("test")
+})
+assert.throws(() => {
+    butWait.thereIsMore(true)
+})
+
+// finish
+butWait.noMore().then(() => {
+    assert.ok(flag === 1);
+    flag = 2;
+});
+
+// throw if noMore was called already
+assert.throws(() => {
+    butWait.thereIsMore(() => {})
+})
+
+resolve1();
+new Promise(res => res())
+    .then(() => {
+        assert.ok(flag === 1);    
+    })
+    .then() // noMore() gets resolved
+    .then(() => {
+        assert.ok(flag === 2);    
+    })
